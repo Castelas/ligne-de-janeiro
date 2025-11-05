897b8ac7692646d0c83c4a105fe527c4ef35dcba
Thiago Narcizo <thiago@narcizo.xyz>
2025-11-05 09:14:50 +0100

    new server ip

--- basic_infrastructure/server.py
+++ basic_infrastructure/server.py
@@ -1,94 +1,52 @@
-#########################################################################
-# CENTRALESUPELEC : ST5 Integration teaching
-#
-# Communication server
-#   This program receives messages from all clients (control and robots).
-#   It forwards messages from the control to all robots.
-#   Each robot filters out messages that are not meant for it.
-#
-#########################################################################
-# Authors : Philippe Benabes & Koen de Turck
-# Modifications by Morgan Roger & Erwan Libessart
-# TBD : nodes is not declared as global
-# TBD : replace 'key' with 'last_key_hit' ?
-#########################################################################
-
+# server.py (sem alterações)
 import zmq
 import sys
 
-server_ip = "192.168.137.7"
-verbose_mode = False
-nodes = list()
-global key # last key hit ?
-key = '' 
+# ---> MUDE AQUI PARA O IP DO COMPUTADOR QUE RODA ESTE SERVIDOR <---
+server_ip = "192.168.137.7" 
 
+verbose_mode = True
+nodes = list()
+global key 
+key = '' 
 
 def main():
     repsock = create_connection_interface(server_ip)
+    print(f"Servidor iniciado em {server_ip}. Aguardando conexoes...")
     while True:
         msg = repsock.recv_pyobj()
         reply = process_msg(msg)
         repsock.send_pyobj(reply)
         if verbose_mode:
-            print("received: {}".format(msg))
-            print("reply: {}".format(reply))
-
+            print(f"Recebido: {msg} -> Respondido: {reply}")
 
 def process_msg(msg):
     global key
-    default_reply = {"all": "is fine"}
+    default_reply = {"status": "ok"}
+    msg_type = msg.get("cmd", "")
+    msg_from = msg.get("from", "")
 
-    msg_type = {
-      "new node registering": ((msg["cmd"] == "log") and msg["from"] not in nodes),
-      "known node registering": ((msg["cmd"] == "log") and msg["from"] in nodes),
-      "from control": (msg["from"] == "control"),
-      "key request from robot": ((msg["from"] != "control") and (msg["cmd"] == "key"))
-               }
-
-    if msg_type["new node registering"]:
-        print("new node signing in, adding {} to nodes".format(msg["from"]))
-        nodes.append(msg["from"])
-        if verbose_mode:
-            print(nodes)
+    if msg_type == "log":
+        if msg_from not in nodes:
+            print(f"Novo no '{msg_from}' se registrando.")
+            nodes.append(msg_from)
         reply = default_reply
-
-    elif msg_type["known node registering"]:
-        print("node {} already known".format(msg["from"]))
-        if verbose_mode:
-            print(nodes)
-        reply = {"already": "registered"}
-
-
-    elif msg_type["from control"]:
-        print("from control")
-        if verbose_mode:
-            print(msg)
-        if (msg["cmd"] == "key"): 
-            key = msg["key"]
-            print(key)
+    elif msg_from == "control" and msg_type == "key":
+        key = msg.get("key", '')
         reply = default_reply
-
-    elif msg_type["key request from robot"]:
-        print("key request from {}".format(msg["from"]))
+    elif msg_from != "control" and msg_type == "key_request":
         reply = {"key": key}
-        key = ''
-
+        key = '' # Limpa a tecla após ser lida pelo robô
     else:
-        print("invalid message")
-        reply = {"message": "is invalid"}
-
-    if verbose_mode:
-        print(reply)
+        reply = {"error": "mensagem invalida"}
     return reply
 
 def create_connection_interface(ip):
     ctx = zmq.Context()
     repsock = ctx.socket(zmq.REP)
-    repaddr = "tcp://{}:5005".format(ip)
+    repaddr = f"tcp://{ip}:5005"
     repsock.bind(repaddr)
-    
     return repsock
-
 
 if __name__ == "__main__":
     main()
