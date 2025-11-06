35592ba183845443555a79759b3d942de826334a
brunadeassis <bruna.deassis@student-cs.fr>
2025-11-06 09:47:27 +0100

    ip bruna

--- basic_infrastructure/server.py
+++ basic_infrastructure/server.py
@@ -3,7 +3,7 @@ import zmq
 import sys
 
 # ---> MUDE AQUI PARA O IP DO COMPUTADOR QUE RODA ESTE SERVIDOR <---
-server_ip = "192.168.137.165" 
+server_ip = "192.168.137.22" 
 
 verbose_mode = True
 nodes = list()
